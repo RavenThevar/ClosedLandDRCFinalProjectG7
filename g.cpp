e76f1e83b1a4422bdae5d441c32960442b633345
Raven Thevar <raven_genius@live.com>
2022-02-14 10:20:50 +0800

    Completing Library Initialisations

--- g.cpp
+++ g.cpp
@@ -1,12 +1,16 @@
 //Libraries Initializations
 #include <iostream>
-// IOStream declares cin, cout, cerr, clog
+// IOStream declares the following: [cin, cout, cerr,clog] and the wide-character counterparts. Note that it is not just a combination of <istream> and <ostream>
 #include <fstream>
+// Defines the basic_filebuf, basic_ifstream, basic_ofstream, and basic_fstream classes. Declares the filebuf, wfilebuf, ifstream, wifstream, ofstream wofstream, fstream, and wfstream typedefs
 #include <iomanip>
+// Declares the I/O manipulators not declared elsewhere(mostly in <ios>)
 #include <vector>
+// The vector class template and the vector<bool> specialization
 #include <cmath>
+// Math Utilities, including trigonometric functions, srqt(), fabs(), and others
 #include <algorithm>
-
+// Prototypes for most of the algorithms in the STL(Standard Template Library)
 // Using Namespace STD
 using namespace std;
-
+// the above allows a Developer to opt out from using STD:: every time
