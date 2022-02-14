250566a2fb543ec9e2ad92399f2770064319b79a
Raven Thevar <raven_genius@live.com>
2022-02-14 10:06:51 +0800

    Declaring I/O Streams Standard Library Headers

---
+++ g.cpp
@@ -0,0 +1,12 @@
+//Libraries Initializations
+#include <iostream>
+// IOStream declares cin, cout, cerr, clog
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+// Using Namespace STD
+using namespace std;
+
