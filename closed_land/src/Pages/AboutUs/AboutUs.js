17b7ebae1e0b07904223a09aa0688ced8e027a30
Luqman Nurhakim <luqman@besquare.com.my>
2022-02-16 17:24:40 +0800

    merge

---
+++ closed_land/src/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+      <div className="hello"></div>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ closed_land/src/Pages/SignIn/SignIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SignInStyle.css";
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1 className="title">SIGN IN</h1>
+      <div className="container">
+        <div className="rectangle1"></div>
+        <div className="rectangle2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
