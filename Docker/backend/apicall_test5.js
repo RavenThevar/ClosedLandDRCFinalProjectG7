9eae6e471767fcbf50fe9461e46e69f68d7df862
Raven Thevar <raven_genius@live.com>
2022-02-16 09:20:39 +0800

    Multiple api_call updates

--- Docker/backend/apicall_test5.js
+++ Docker/backend/apicall_test5.js
@@ -1,15 +1,13 @@
-const { Stats } = require("fs");
 const https = require("https");
 
 let j = 0;
 const mtov = [];
+const ltov = [];
+let offsetty = 300;
 
 https
   .get(
-    //"https://api.opensea.io/api/v1/collection/doodles-official/stats",
-    //"https://api.opensea.io/api/v1/collection",
-    //"https://api.opensea.io/api/v1/collection/doodles-official",
-    "https://api.opensea.io/api/v1/collections?limit=300",
+    `https://api.opensea.io/api/v1/collections?offset=${offsetty}&limit=300`,
     (resp) => {
       let data = "";
 
@@ -24,16 +22,16 @@ https
         console.log("          ***************************************");
         console.log("          |  All Collections as the Following:  |");
         console.log("          ***************************************");
-        //console.log(Object.keys(objData.collections[18]));
-        //console.log(Object.keys(objData));
 
         for (i = 0; i < 300; i++) {
-          //   let rubbles = objData.collections[i].stats.one_day_volume;
-          //   console.log(rubbles);
           if (objData.collections[i].stats.one_day_volume != 0) {
             mtov[j] = objData.collections[i].stats.one_day_volume;
             j++;
             console.log("added");
+            console.log(mtov);
+          } else {
+            ltov[j] = objData.collections[i].stats.one_day_volume;
+            console.log(ltov);
           }
         }
       });
