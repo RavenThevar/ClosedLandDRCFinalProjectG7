251157666cbe53716626345610f6c555df2138b9
Raven Thevar <raven_genius@live.com>
2022-02-22 11:50:38 +0800

    Testing Out Redis Streaming Features

--- Docker/backend/apiredisstreamtest.js
+++ Docker/backend/apiredisstreamtest.js
@@ -12,8 +12,10 @@ const faglover = async function () {
 
   // specify channel to write a message into,
   // messages are key value
-  const myMessage = "hello world";
-  await redis.xadd(channel, "*", myMessage, "message");
+  const myKey = "hello world";
+  const myValue = "LOLOLOL";
+
+  await redis.xadd(channel, "*", myKey, myValue);
 
   messageCount = await redis.xlen(channel);
   console.log(
@@ -24,15 +26,20 @@ const faglover = async function () {
   // use xread to read all messages in channel
   let messages = await redis.xread(["STREAMS", channel, 0]);
   messages = messages[0][1];
+  console.log("SEE THIS " + messages);
+  console.log("SEE THIS 2 " + messages[0][1]);
+  console.log("SEE THIS 3 " + messages[0]);
+  console.log("SEE THIS 4 " + messages[1]);
+  console.log("SEE THIS 5 " + messages[0][1][0]);
   console.log(
     `reading messages from channel ${channel}, found ${messages.length} messages`
   );
   for (let i = 0; i < messages.length; i++) {
     let msg = messages[i];
-    msg = msg[1][0].toString();
+    //msg = msg[1][0].toString();
+    msg = msg[1][0];
     console.log("reading message:", msg);
   }
   process.exit(0);
 };
-
 faglover();
