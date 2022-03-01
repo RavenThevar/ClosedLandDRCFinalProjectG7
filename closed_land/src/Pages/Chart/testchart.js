e0ac25919eca89b41fb0d633e89d9d350a0c19ff
myrabaharom <mira.bahar57@gmail.com>
2022-03-01 09:48:33 +0800

    updated css

--- closed_land/src/Pages/Chart/testchart.js
+++ closed_land/src/Pages/Chart/testchart.js
@@ -47,26 +47,26 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-export const data = {
-  labels,
-  datasets: [],
-    {
-      label: 'Line Chart',
-      data: 
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    }
-    // {
-    //   label: 'Dataset 2',
-    //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-    //   borderColor: 'rgb(53, 162, 235)',
-    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    // },
-  ],
-};
+// export const data = {
+//   labels,
+//   datasets: [],
+//     // {
+//     //   label: 'Line Chart',
+//     //   data: 
+//     //   borderColor: 'rgb(255, 99, 132)',
+//     //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//     // }
+//     // {
+//     //   label: 'Dataset 2',
+//     //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+//     //   borderColor: 'rgb(53, 162, 235)',
+//     //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//     // },
+//   ],
+// };
 
 export const Testcharts =()=> {
-  return <Line options={options} data={data} />;
+  // return <Line options={options} data={data} />;
 }
