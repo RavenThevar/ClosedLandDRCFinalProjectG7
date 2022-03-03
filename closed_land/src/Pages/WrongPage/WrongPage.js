65fa30905fc73f7def25cc7d33407e004c3e235a
Ameerul Zul Hadi <ameerulzhbusiness@gmail.com>
2022-03-03 10:49:01 +0800

    updated responsiveness

--- closed_land/src/Pages/WrongPage/WrongPage.js
+++ closed_land/src/Pages/WrongPage/WrongPage.js
@@ -7,9 +7,16 @@ import loadingImg from "../images/loading.svg";
 const WrongPage = () => {
   return (
     <div className="wrongPage">
-      <h1 className="fourOfour">Error 404!</h1>
-      <h1>Page Doesn't Exist.</h1>
-      <img src={loadingImg} />
+      <h1 className="fourOfour animate__animated animate__zoomInLeft animate__delay-1s">
+        Error 404!
+      </h1>
+      <h1 className="animate__animated animate__zoomInDown animate__delay-1s">
+        Page Doesn't Exist.
+      </h1>
+      <img
+        src={loadingImg}
+        // className="animate__animated animate__zoomInDown animate__delay-1s"
+      />
       <div className="wrongPageCube">{/* <LogoCube2></LogoCube2> */}</div>
     </div>
   );
