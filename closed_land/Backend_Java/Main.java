7761f664b5fcfa06f1f5efecbf02718ab4cee9fd
RavenThevar <raven_genius@live.com>
2022-04-14 23:39:07 +0800

    Commenting out this file for temporary purpose

--- closed_land/Backend_Java/Main.java
+++ closed_land/Backend_Java/Main.java
@@ -1,70 +1,70 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+// import org.json.simple.JSONArray;
+// import org.json.simple.JSONObject;
+// import org.json.simple.parser.JSONParser;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
+// import java.net.HttpURLConnection;
+// import java.net.URL;
+// import java.util.Scanner;
 
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        /*
-        Maven dependency for JSON-simple:
-            <dependency>
-                <groupId>com.googlecode.json-simple</groupId>
-                <artifactId>json-simple</artifactId>
-                <version>1.1.1</version>
-            </dependency>
-         */
+//         /*
+//         Maven dependency for JSON-simple:
+//             <dependency>
+//                 <groupId>com.googlecode.json-simple</groupId>
+//                 <artifactId>json-simple</artifactId>
+//                 <version>1.1.1</version>
+//             </dependency>
+//          */
 
-        try {
-            //Public API:
-            //https://www.metaweather.com/api/location/search/?query=<CITY>
-            //https://www.metaweather.com/api/location/44418/
+//         try {
+//             //Public API:
+//             //https://www.metaweather.com/api/location/search/?query=<CITY>
+//             //https://www.metaweather.com/api/location/44418/
 
-            URL url = new URL("https://www.metaweather.com/api/location/search/?query=London");
+//             URL url = new URL("https://www.metaweather.com/api/location/search/?query=London");
 
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
+//             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//             conn.setRequestMethod("GET");
+//             conn.connect();
 
-            //Check if connect is made
-            int responseCode = conn.getResponseCode();
+//             //Check if connect is made
+//             int responseCode = conn.getResponseCode();
 
-            // 200 OK
-            if (responseCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + responseCode);
-            } else {
+//             // 200 OK
+//             if (responseCode != 200) {
+//                 throw new RuntimeException("HttpResponseCode: " + responseCode);
+//             } else {
 
-                StringBuilder informationString = new StringBuilder();
-                Scanner scanner = new Scanner(url.openStream());
+//                 StringBuilder informationString = new StringBuilder();
+//                 Scanner scanner = new Scanner(url.openStream());
 
-                while (scanner.hasNext()) {
-                    informationString.append(scanner.nextLine());
-                }
-                //Close the scanner
-                scanner.close();
+//                 while (scanner.hasNext()) {
+//                     informationString.append(scanner.nextLine());
+//                 }
+//                 //Close the scanner
+//                 scanner.close();
 
-                System.out.println(informationString);
+//                 System.out.println(informationString);
 
 
-                //JSON simple library Setup with Maven is used to convert strings to JSON
-                JSONParser parse = new JSONParser();
-                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
+//                 //JSON simple library Setup with Maven is used to convert strings to JSON
+//                 JSONParser parse = new JSONParser();
+//                 JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
 
-                //Get the first JSON object in the JSON array
-                System.out.println(dataObject.get(0));
+//                 //Get the first JSON object in the JSON array
+//                 System.out.println(dataObject.get(0));
 
-                JSONObject countryData = (JSONObject) dataObject.get(0);
+//                 JSONObject countryData = (JSONObject) dataObject.get(0);
 
-                System.out.println(countryData.get("woeid"));
+//                 System.out.println(countryData.get("woeid"));
 
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
 
-}
+// }
